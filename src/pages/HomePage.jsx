ad8dc97fd913f6d5689349eeed4dd932cd251ad5
Ashley Oliver <jeangray218@gmail.com>
2023-05-27 09:54:08 -0400

    added nav bar and homepage and minor styling

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div>
+        <h1>HomePage</h1>
+        <p>Welcome to a place where you can electronically express all of you thoughts, hopes, dreams, and goals</p>
+    </div>
+  )
+}
